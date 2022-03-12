9775ed7902f2dfddb1240ac2f398b519d5e309f7
Jeong Woojin <doublexuj@gmail.com>
2022-03-12 15:10:27 +0900

    Add Compile SCSS

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,20 +1,62 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
+import ws from "gulp-webserver";
+import image from "gulp-image";
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+import autop from "gulp-autoprefixer";
+import miniCSS from "gulp-csso";
+
+const sass = gulpSass(dartSass);
 
 const routes = {
   pug: {
+    watch: "src/**/*.pug",
     src: "src/*.pug",
-    dest: "build"
-  }
-}
+    dest: "build",
+  },
+  img: {
+    src: "src/img/*",
+    dest: "build/img",
+  },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css",
+  },
+};
 
-const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+const pug = () =>
+  gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
 const clean = () => del(["build"]);
 
-const prepare = gulp.series([clean]);
+const webserver = () =>
+  gulp.src("build").pipe(ws({ livereload: true, open: true }));
 
-const assets = gulp.series([pug]);
+const img = () =>
+  gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
 
-export const dev = gulp.series([prepare, assets]);
+const styles = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autop())
+    .pipe(miniCSS())
+    .pipe(gulp.dest(routes.scss.dest));
+
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.img.src, img);
+  gulp.watch(routes.scss.watch, styles);
+};
+
+const prepare = gulp.series([clean, img]);
+
+const assets = gulp.series([pug, styles]);
+
+// 두 가지 Task를 동시에 실행하기 위해서는 parallel 사용
+const postDev = gulp.parallel([webserver, watch]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
